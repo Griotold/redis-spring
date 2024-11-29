645e0e4c83bad857e14bcd2f371d16e0fb1cfe45
Griotold <gotjd9773@naver.com>
2024-11-29 14:12:09 +0900

    id Long으로 했을 때 id값을 넣어주지 않으면 랜덤한 값이 들어가고(예: -471012825455506325), id를 지정(예: 1L) 하면 지정한 값이 들어간다.
    hash tables에
    item:-471012825455506325
    item:1
    이렇게 들어가지게 되는 것이다.

---
+++ src/main/java/com/study/redis/Item.java
@@ -0,0 +1,24 @@
+package com.study.redis;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+// Entity 대신 RedisHash
+@RedisHash("item") // "item" 이라는 key
+public class Item implements Serializable {
+
+    @Id
+    // Id String 으로 하면 UUID 자동으로 배정된다.
+    private Long id;
+    private String name;
+    private String description;
+    private Integer price;
+}
---
+++ src/test/java/com/study/redis/RedisRepositoryTests.java
@@ -0,0 +1,42 @@
+package com.study.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RedisRepositoryTests {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Test
+    void createTest() {
+        // 객체를 만들고
+        Item item = Item.builder()
+                .id(1L)
+                .name("keyboard")
+                .description("Very Expensive Keyboard")
+                .price(100_000)
+                .build();
+        // save를 호출한다.
+        itemRepository.save(item);
+    }
+
+    @Test
+    void redOneTest() {
+        Item item = itemRepository.findById(1L)
+                .orElseThrow();
+
+        System.out.println(item.getDescription());
+    }
+
+    @Test
+    void updateTest() {
+        Item item = itemRepository.findById(1L)
+                .orElseThrow();
+        item.setDescription("On Sale!!");
+        item = itemRepository.save(item);
+        System.out.println(item.getDescription());
+    }
+}
