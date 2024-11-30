e335275e5c6d247d4fffbba51265080ce4e3f50b
Griotold <gotjd9773@naver.com>
2024-11-30 12:56:39 +0900

    [Cache] readOne 메서드에 캐시 적용하기 - Cache-Aside 방식

--- src/main/java/com/study/redis/cache/ItemServiceForCache.java
+++ src/main/java/com/study/redis/cache/ItemServiceForCache.java
@@ -3,6 +3,7 @@ package com.study.redis.cache;
 import com.study.redis.cache.domain.ItemDtoForCache;
 import com.study.redis.cache.domain.ItemForCache;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -21,9 +22,9 @@ public class ItemServiceForCache {
 
     public ItemDtoForCache create(ItemDtoForCache dto) {
         return ItemDtoForCache.from(itemRepository.save(ItemForCache.builder()
-                        .name(dto.getName())
-                        .description(dto.getDescription())
-                        .price(dto.getPrice())
+                .name(dto.getName())
+                .description(dto.getDescription())
+                .price(dto.getPrice())
                 .build()));
     }
 
@@ -34,7 +35,15 @@ public class ItemServiceForCache {
                 .toList();
     }
 
+    /**
+     * 이 메서드의 결과는 캐싱이 가능하다.
+     * cacheNames: 적용할 캐시 규칙을 지정하기 위한 이름
+     * key = "args[0]" : 0번째 파라미터를 key(캐시 데이터를 구분하기 위해 활용하는 값)로 쓰겠다.
+     * Cache-Aside 방식
+     */
+    @Cacheable(cacheNames = "itemCache", key = "args[0]")
     public ItemDtoForCache readOne(Long id) {
+        log.info("Read One: {}", id);
         return itemRepository.findById(id)
                 .map(ItemDtoForCache::from)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
